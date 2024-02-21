fad83d278f94d6d8769870d81290103fd53d5b35
Sergio Amo <sergio.amo@outlook.com>
2024-02-21 19:16:36 +0100

    Initial Commit

---
+++ Marvel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Sergio Amo on 21/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Sergio Amo on 21/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
