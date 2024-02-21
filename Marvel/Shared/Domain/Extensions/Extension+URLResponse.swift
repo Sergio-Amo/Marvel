99be30694137e7f61813147f6d6734ff24e8abef
Sergio Amo <sergio.amo@outlook.com>
2024-02-21 22:20:40 +0100

    Create Extension+URLResponse.swift
    Create URLResponse statusCode extension and proper documentation comments

---
+++ Marvel/Shared/Domain/Extensions/Extension+URLResponse.swift
@@ -0,0 +1,22 @@
+//
+//  Extension+URLResponse.swift
+//  Marvel
+//
+//  Created by Sergio Amo on 21/2/24.
+//
+
+import Foundation
+
+extension URLResponse {
+    /// Computed property that returns the status code of a HTTP response.
+    /// 
+    /// - returns: Integer with the StatusCode or nil if fails
+    /// # Usage #
+    /// ```
+    /// let statusCode = response.statusCode
+    /// ```
+    
+    var statusCode: Int? {
+        return (self as? HTTPURLResponse)?.statusCode
+    }
+}
