5f82016c6d8522d6f59e54629c339f9c20ecc852
Sergio Amo <sergio.amo@outlook.com>
2024-02-22 19:07:16 +0100

    Make marvelitem title and name optional during initialization

--- Marvel/Shared/Domain/Models/MarvelResponse.swift
+++ Marvel/Shared/Domain/Models/MarvelResponse.swift
@@ -26,6 +26,15 @@ struct MarvelItem: Codable, Identifiable {
     let title: String? // Series have title instead of name
     let description: String?
     let thumbnail: Thumbnail?
+    // The things one must to to have an optional value that doesn't need to be explicity initialized...
+    init(id: Int?, name: String? = nil, title: String? = nil, description: String?, thumbnail: Thumbnail?) {
+        self.id = id
+        self.name = name
+        self.title = title
+        self.description = description
+        self.thumbnail = thumbnail
+    }
+    
 }
 
 // MARK: - Thumbnail
@@ -34,7 +43,7 @@ struct Thumbnail: Codable {
     // Extension as string instead of enum so it'll work fine even if at some point they insert other formats like heic
     // Shouldn't be a problem as far as xCode support the format.
     let thumbnailExtension: String?
-
+    
     enum CodingKeys: String, CodingKey {
         case path
         case thumbnailExtension = "extension"
