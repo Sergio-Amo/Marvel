7695d98bb514d5e38f980b3a77df8695f83cd7c4
Sergio Amo <sergio.amo@outlook.com>
2024-03-02 22:22:30 +0100

    Trying to test networker, not yet there...

--- Marvel/Shared/Data/Network/Networker.swift
+++ Marvel/Shared/Data/Network/Networker.swift
@@ -12,10 +12,23 @@ protocol NetworkerProtocol {
     func callServer(request: URLRequest) -> AnyPublisher<MarvelResponse, Error>
 }
 
+protocol URLSessionProtocol {
+    func dataTaskPublisher(for request: URLRequest) -> URLSession.DataTaskPublisher
+}
+extension URLSession: URLSessionProtocol {
+    static var defaultSession: URLSessionProtocol {
+        return URLSession(configuration: .default)
+    }
+}
+
 final class Networker: NetworkerProtocol {
+    private let session: URLSessionProtocol
+    init(session: URLSessionProtocol = URLSession(configuration: .default)) {
+        self.session = session
+    }
     // No need for generics as all calls are decoded to the same type
     func callServer(request: URLRequest) -> AnyPublisher<MarvelResponse, Error> {
-        URLSession.shared
+        session
             .dataTaskPublisher(for: request)
             .tryMap {
                 guard $0.response.computedStatusCode == 200 else {
---
+++ MarvelTests/Data/Network/NetworkerTests.swift
@@ -0,0 +1,73 @@
+//
+//  NetworkerTests.swift
+//  MarvelTests
+//
+//  Created by Sergio Amo on 2/3/24.
+//
+
+import XCTest
+import Combine
+@testable import Marvel
+
+class NetworkerTests: XCTestCase {
+    var suscriptors = Set<AnyCancellable>()
+    
+    func testCallServer() {
+        let publisher: AnyPublisher<MarvelResponse, Error> = FakeMarvelInteractor().getSeries(id: 1, offset: 0)
+        // Create a mock URLSession
+        let mockSession: URLSession = MockURLSession(publisher: publisher)
+        
+        // Create the Networker instance with the mock URLSession
+        let networker = Networker(session: mockSession)
+        
+        // Create a URLRequest
+        let request = URLRequest(url: URL(string: "https://www.example.com")!)
+        
+        // Call networker
+        networker.callServer(request: request)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                    case .failure(let error):
+                        XCTFail("\(error)")
+                    case .finished:
+                        break
+                }
+            }, receiveValue: { value in
+               // TODO: Do some asserts to test everything is ok
+            })
+            .store(in: &suscriptors)
+    }
+}
+
+final class MockURLSession: URLSessionProtocol {
+    var publisher: AnyPublisher<MarvelResponse, any Error>
+    
+    init(publisher: AnyPublisher<MarvelResponse, any Error>) {
+        self.publisher = publisher
+    }
+    
+    func dataTaskPublisher(for request: URLRequest) -> URLSession.DataTaskPublisher {
+        // I don't know how to return my publisher as URLSession.DataTaskPublisher
+    }
+}
+
+
+
+
+
+
+
+
+/*
+// Mock URLSession
+class MockURLSession: URLSession {
+    var publisher: AnyPublisher<MarvelResponse, any Error>
+
+    init(publisher: AnyPublisher<MarvelResponse, any Error>) {
+        self.publisher = publisher
+    }
+    func dataTaskPublisher(for request: URLRequest) -> URLSession.DataTaskPublisher {
+        return URLSession.DataTaskPublisher(publisher: publisher)
+    }
+}
+*/
