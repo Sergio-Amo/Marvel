716408fb3beefc8f2fe18b7c6e89027aee306fd2
Sergio Amo <137457730+Sergio-Amo@users.noreply.github.com>
2024-03-03 19:49:01 +0100

    Update Authentication placeholder to contain its protocol

--- Marvel/Authentication.swift
+++ Marvel/Authentication.swift
@@ -8,6 +8,10 @@
 import Foundation
 import CryptoKit
 
+protocol AuthenticationProtocol {
+    var authParams: String { get }
+}
+
 struct Authentication {
     private let publicKey = "INSERT_YOUR_PUBLIC_KEY_HERE"
     private let privateKey = "INSERT_YOUR_PRIVATEKEY_KEY_HERE"
@@ -20,7 +24,6 @@ struct Authentication {
     
     private func string2Md5 (string: String) -> String {
         let digest = Insecure.MD5.hash(data: Data(string.utf8))
-        
         return digest.map {
             // 02 : String width = 2 add 0 at the front if width is less than 2
             // hhx: x for hex, hh as lenght modifier specifying that the hex conversion applies to a char
